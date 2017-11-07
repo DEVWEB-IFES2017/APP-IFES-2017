cc318e2cc2779a5bb2ccff55a6b3f7e37deaed6f
Alexandre Pesqueira <alexandrepesqueira@gmail.com>
2017-11-07 21:46:59 -0200

    Criação da Controller Usuário API

--- AppIFES/App_Start/RouteConfig.cs
+++ AppIFES/App_Start/RouteConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -16,8 +17,14 @@ namespace AppIFES
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}/{id2}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional , id2 = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, id2 = UrlParameter.Optional }
             );
+            /*routes.MapHttpAttributeRoutes();
+            routes.Routes.MapHttpRoute(
+                name: "WebApi",
+                routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+            );*/
         }
     }
 }
