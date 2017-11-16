9d8ff1df5e6003d1ef774d3dba6184a0bb993d14
Alexandre Pesqueira <alexandrepesqueira@gmail.com>
2017-11-16 18:56:33 -0200

    Inclusão do logo no app

--- IFESApp/www/build/main.js
+++ IFESApp/www/build/main.js
@@ -211,7 +211,7 @@ var HomePage = (function () {
 }());
 HomePage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-home',template:/*ion-inline-start:"C:\Users\alexa\Documents\IFES\APP-IFES-2017\IFESApp\src\pages\home\home.html"*/'<ion-header>\n\n  <ion-navbar>\n\n   <ion-title>IFES</ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding class="home">\n\n\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\alexa\Documents\IFES\APP-IFES-2017\IFESApp\src\pages\home\home.html"*/
+        selector: 'page-home',template:/*ion-inline-start:"C:\Users\alexa\Documents\IFES\APP-IFES-2017\IFESApp\src\pages\home\home.html"*/'<ion-header>\n\n    \n\n  <ion-navbar>\n\n      <ion-avatar item-start>\n\n      <img alt="logo" height="40" src="../assets/imgs/LogoIFES.png">\n\n      </ion-avatar>\n\n   <ion-title>IFES</ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding class="home">\n\n\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\alexa\Documents\IFES\APP-IFES-2017\IFESApp\src\pages\home\home.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]])
 ], HomePage);
@@ -484,7 +484,7 @@ var ServiceLogin = (function () {
 }());
 ServiceLogin = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["B" /* Injectable */])(),
-    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_3__database_database__["a" /* DatabaseProvider */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_3__database_database__["a" /* DatabaseProvider */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */]) === "function" && _b || Object])
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_3__database_database__["a" /* DatabaseProvider */], __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */]])
 ], ServiceLogin);
 
 var Usuario = (function () {
@@ -493,7 +493,6 @@ var Usuario = (function () {
     return Usuario;
 }());
 
-var _a, _b;
 //# sourceMappingURL=servicelogin.js.map
 
 /***/ }),
